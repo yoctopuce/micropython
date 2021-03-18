321c4cfbe3d3a99ce2ed2dc58caa724b0cb988a3
srinsoz <sebastien@rinsoz.org>
2021-03-18 12:46:32 +0100

    # This is a combination of 6 commits. # This is the 1st commit message:
    msp432: new port for Texas Instrument MSP432E401Y
    
    # The commit message #2 will be skipped:
    
    # msp432: Fix compilation of MSP432 port
    
    # The commit message #3 will be skipped:
    
    # msp432: fix code formating
    
    # The commit message #4 will be skipped:
    
    # msp432: cleanup makefile
    
    # The commit message #5 will be skipped:
    
    # msp432 more code formating
    
    # The commit message #6 will be skipped:
    
    # msp432: more code formating

---
+++ ports/msp432/mkconf.mk
@@ -0,0 +1,20 @@
+SIMPLELINK_MSP432E4_SDK_INSTALL_DIR ?= c:\\ti\\simplelink_msp432e4_sdk_4_20_00_12
+FREERTOS_INSTALL_DIR   ?= E:\\data\\yoctopuce\\yoctoprod\\FreeRTOS\\FreeRTOSv10.2.1
+CCS_INSTALL_DIR        ?= c:\\ti\\ccs901\\ccs
+SYSCONFIG_TOOL         ?= $(CCS_INSTALL_DIR)/utils/sysconfig/cli.js
+GCC_ARMCOMPILER        ?= $(CCS_INSTALL_DIR)/tools/compiler/gcc-arm-none-eabi-7-2017-q4-major-win32
+
+PYTHON = python.exe
+
+ifeq ($(OS),Windows_NT)
+XDCTOOLS ?= $(CCS_INSTALL_DIR)\\xdctools_3_55_00_11_core\\bin\\
+CAT = "$(XDCTOOLS)cat"
+RM = "$(XDCTOOLS)rm"
+MKDIR = "$(XDCTOOLS)mkdir"
+SED = "$(XDCTOOLS)sed"
+TOUCH = "$(XDCTOOLS)touch"
+AR = "$(GCC_ARMCOMPILER)/bin/arm-none-eabi-ar"
+
+endif
+
+
